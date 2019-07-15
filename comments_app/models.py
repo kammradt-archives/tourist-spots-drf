704152f88db53fed95a0eb12237e3c5a322d1efb
kammradt <vinicius.kammradt1@gmail.com>
2019-07-15 16:36:10 -0300

    :bug: Adding default values

--- comments_app/models.py
+++ comments_app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     available = models.BooleanField(default=True)
 
     def __str__(self):
--- tourist_spots_app/models.py
+++ tourist_spots_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from attractions_app.models import Attraction
 from comments_app.models import Comment
+from reviews_app.models import Review
 
 
 class TouristSpots(models.Model):
@@ -9,6 +10,7 @@ class TouristSpots(models.Model):
     available = models.BooleanField(default=False)
     attractions = models.ManyToManyField(Attraction)
     comments = models.ManyToManyField(Comment)
+    reviews = models.ManyToManyField(Review)
 
     def __str__(self):
         return self.name
