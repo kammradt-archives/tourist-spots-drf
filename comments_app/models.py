efc44723e0935341f2a0ca6f97859543067ca748
kammradt <vinicius.kammradt1@gmail.com>
2019-07-15 16:10:32 -0300

    :sparkles: Creating 'comments' app

---
+++ comments_app/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.user.first_name}: {self.content}'
--- tourist_spots/settings.py
+++ tourist_spots/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'tourist_spots_app',
     'attractions_app',
+    'comments_app'
 ]
 
 MIDDLEWARE = [
