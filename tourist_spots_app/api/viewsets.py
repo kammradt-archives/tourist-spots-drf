d22adf9f23ee61be597dbe3a28ff9edc5ae718da
kammradt <vinicius.kammradt1@gmail.com>
2019-07-18 01:23:24 -0300

    :construction: Learning how to use custom actions
    DRF let us create custom actions that can be called using custom URLs. In this case, we created a simple one that need to be implemented. The ideia is to create a report system to help admins moderate content.

--- tourist_spots_app/api/viewsets.py
+++ tourist_spots_app/api/viewsets.py
@@ -1,3 +1,4 @@
+from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
 from .serializers import TouristSpotsSerializer
 from tourist_spots_app.models import TouristSpot
@@ -8,4 +9,11 @@ class TouristSpotViewSet(ModelViewSet):
 
     def get_queryset(self):
         # Better filters can be added here
-        return TouristSpot.objects.filter(available=True)
+        return TouristSpot.objects.filter(available=False)
+
+    @action(methods=['post'], detail=True)
+    def report(self, request, pk=None):
+        # We will create a report page (form) for users that for some
+        # reason want to report a SPECIFIC TouristSpot and then
+        # the admins will verify
+        pass
