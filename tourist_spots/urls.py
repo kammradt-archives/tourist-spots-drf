3ebd0a8cef6456da05dcbd1e638e603ac17ca3c1
kammradt <vinicius.kammradt1@gmail.com>
2019-07-18 01:26:26 -0300

    :wrench: Setting up django to handle imgs/media
    Some extra configuration is needed to handle media as we were just using Plain-Text/JSON before.

--- tourist_spots/urls.py
+++ tourist_spots/urls.py
@@ -1,7 +1,9 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
+from django.conf import settings
 from tourist_spots_app.api.viewsets import TouristSpotViewSet
 from attractions_app.api.viewsets import AttractionViewSet
 from addresses_app.api.viewsets import AddressViewSet
@@ -10,7 +12,7 @@ from reviews_app.api.viewsets import ReviewViewSet
 
 
 router = routers.DefaultRouter()
-router.register(r'tourist-spots', TouristSpotViewSet)
+router.register(r'tourist-spots', TouristSpotViewSet, base_name='TouristSpot')
 router.register(r'attractions', AttractionViewSet)
 router.register(r'addresses', AddressViewSet)
 router.register(r'comments', CommentViewSet, base_name='Comment')
@@ -20,4 +22,4 @@ router.register(r'reviews', ReviewViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
