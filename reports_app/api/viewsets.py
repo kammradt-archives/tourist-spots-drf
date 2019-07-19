52e4730634bd9d0f59acdd4eb43cb71398e48576
kammradt <vinicius.kammradt1@gmail.com>
2019-07-19 00:23:36 -0300

    :sparkles: Adding filter fields to Report endpoint

--- reports_app/api/viewsets.py
+++ reports_app/api/viewsets.py
@@ -7,4 +7,5 @@ from reports_app.models import Report
 class ReportViewSet(ModelViewSet):
     queryset = Report.objects.all()
     serializer_class = ReportSerializer
+    filter_fields = ['content', 'content_type']
     http_method_names = ['get', 'put', 'delete']
--- reports_app/models.py
+++ reports_app/models.py
@@ -12,7 +12,7 @@ class Report(models.Model):
 
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
-    content_object = GenericForeignKey()
+    content_object = GenericForeignKey('content_type', 'object_id')
 
     def __str__(self):
         return f'{self.user_id}: {self.content}'
