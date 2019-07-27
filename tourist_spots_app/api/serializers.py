4c06dba7bcde1daa7693dcbf33f5cc6b079793a9
kammradt <vinicius.kammradt1@gmail.com>
2019-07-27 14:28:38 -0300

    :wrench: Adding new fields to TouristSpotSerializer
    Previously there were ManyToMany relationships, and now I query for specific objects and add it to the response.

--- tourist_spots_app/api/serializers.py
+++ tourist_spots_app/api/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework.serializers import ModelSerializer
+
+from addresses_app.api.serializers import AddressSerializer
+from attractions_app.api.serializers import AttractionSerializer
+from comments_app.api.serializers import CommentSerializer
+from reviews_app.api.serializers import ReviewSerializer
 from tourist_spots_app.models import TouristSpot
 
 
 class TouristSpotsSerializer(ModelSerializer):
+    attractions = AttractionSerializer(source='attraction_set', many=True)
+    comments = CommentSerializer(source='comment_set', many=True)
+    reviews = ReviewSerializer(source='review_set', many=True)
+    address = AddressSerializer()
+
     class Meta:
         model = TouristSpot
         fields = '__all__'
-        depth = 1
