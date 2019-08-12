cfb4ebb6eef3b969d1767d17e3a38b5fbf3468f9
kammradt <vinicius.kammradt1@gmail.com>
2019-08-12 20:53:16 -0300

    :card_file_box: Fixing a 'default value' bug
    Now the 'available' field is correct, and when the tourist spot is created, 'available' is set to True by default. It needed the 'blank=True' option, otherwise it would create with a different default value.

--- tourist_spots_app/models.py
+++ tourist_spots_app/models.py
@@ -7,7 +7,7 @@ class TouristSpot(models.Model):
 
     name = models.CharField(max_length=250)
     description = models.CharField(max_length=500)
-    available = models.BooleanField(default=True)
+    available = models.BooleanField(blank=True, default=True)
     photo = models.ImageField(upload_to='tourist_spots_images', blank=True)
 
     def __str__(self):
