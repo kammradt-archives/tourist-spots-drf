a4f1e38b704442dafe786cb13c5e6ce366f64fbc
kammradt <vinicius.kammradt1@gmail.com>
2019-08-31 18:59:26 -0300

    :sparkles: Adding user to TouristSpot when created

--- tourist_spots_app/api/serializers.py
+++ tourist_spots_app/api/serializers.py
@@ -1,18 +1,27 @@
 from rest_framework.serializers import ModelSerializer
 
 from addresses_app.api.serializers import AddressSerializer
+from addresses_app.models import Address
 from attractions_app.api.serializers import AttractionSerializer
 from comments_app.api.serializers import CommentSerializer
 from reviews_app.api.serializers import ReviewSerializer
 from tourist_spots_app.models import TouristSpot
+from users_app.api.serializers import UserSerializer
 
 
 class TouristSpotsSerializer(ModelSerializer):
-    attractions = AttractionSerializer(source='attraction_set', many=True)
-    comments = CommentSerializer(source='comment_set', many=True)
-    reviews = ReviewSerializer(source='review_set', many=True)
-    address = AddressSerializer()
+    user = UserSerializer(required=False, read_only=True)
+    attractions = AttractionSerializer(source='attraction_set', many=True, required=False)
+    comments = CommentSerializer(source='comment_set', many=True, required=False)
+    reviews = ReviewSerializer(source='review_set', many=True, required=False)
+    address = AddressSerializer(required=False)
 
     class Meta:
         model = TouristSpot
         fields = '__all__'
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        spot = TouristSpot.objects.create(**validated_data, user=user)
+        TouristSpot.save(spot)
+        return spot
